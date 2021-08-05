b941468d376c6cd6c0078b3f0a92c54f0060e492
luanvn2203 <59554558+luanvn2203@users.noreply.github.com>
2021-08-06 02:40:58 +0700

    fix nhan qua

--- Controller/DonorServiceRelationAccountController/DonorServiceRelationAccountController.js
+++ Controller/DonorServiceRelationAccountController/DonorServiceRelationAccountController.js
@@ -7,28 +7,24 @@ module.exports = {
     saveRelation: async function (req, res, next) {
         try {
             const emailReceive = req.body.params.email
-            const serviceDetailId = req.body.params.serviceDetailId
+            const donorServiceId = req.body.params.serviceId
             const quantity = req.body.params.quantity
 
-            const serviceDetailFound = await serviceDetailService.getServiceDetailInformationById(serviceDetailId)
-
-
-            if (serviceDetailFound.length > 0) {
-                if (serviceDetailFound[0].statusId !== 4) {
-
-                    const donorServiceFound = await donorServiceService.getServiceById(serviceDetailFound[0].serviceId)
-                    console.log(donorServiceFound[0])
-                    if (donorServiceFound.length > 0) {
-                        if (donorServiceFound[0].serviceTypeId === 3) {
-                            //hien vat
+            const donorServiceFound = await donorServiceService.getServiceById(donorServiceId)
+            if (donorServiceFound.length > 0) {
+                if (donorServiceFound[0].serviceTypeId === 3) {
+                    // hien vat
+                    const serviceDetailFound = await serviceDetailService.getAllDetailByServiceId(donorServiceId)
+                    if (serviceDetailFound.length > 0) {
+                        if (serviceDetailFound[0].statusId !== 4) {
                             const quantityDB = serviceDetailFound[0].quantity
                             if (quantityDB >= quantity) {
-                                await serviceDetailService.updateServiceDetailQuantity(serviceDetailId, (quantityDB - quantity))
+                                await serviceDetailService.updateServiceDetailQuantity(serviceDetailFound[0].id, (quantityDB - quantity))
                                 if (quantityDB === quantity) {
                                     const unavailableStatus = 4
-                                    await serviceDetailService.updateDetailStatusById(serviceDetailId, unavailableStatus)
+                                    await serviceDetailService.updateDetailStatusById(serviceDetailFound[0].id, unavailableStatus)
                                 }
-                                const result = await donorServiceRelationAccountService.saveServiceRelationToAccount(emailReceive, serviceDetailId, quantity)
+                                const result = await donorServiceRelationAccountService.saveServiceRelationToAccount(emailReceive, serviceDetailFound[0].id, quantity)
                                 if (result === true) {
                                     // tru so luong
                                     res.status(200).json({
@@ -42,12 +38,23 @@ module.exports = {
                                     })
                                 }
                             }
-
                         } else {
-                            // code
+                            res.status(202).json({
+                                status: "Failed",
+                                message: "Item is not available: " + donorServiceId
+                            })
+                        }
+                    }
+
+                } else {
+                    // code
+                    const serviceDetailFound = await serviceDetailService.getAllDetailByServiceId(donorServiceId)
+                    if (serviceDetailFound.length > 0) {
+                        const availableServiceDetail = serviceDetailFound.filter((item, index) => item.statusId !== 4)
+                        if (availableServiceDetail.length > 0) {
                             const unavailableStatus = 4
-                            await serviceDetailService.updateDetailStatusById(serviceDetailId, unavailableStatus)
-                            const result = await donorServiceRelationAccountService.saveServiceRelationToAccount(emailReceive, serviceDetailId, 1)
+                            await serviceDetailService.updateDetailStatusById(availableServiceDetail[0].id, unavailableStatus)
+                            const result = await donorServiceRelationAccountService.saveServiceRelationToAccount(emailReceive, availableServiceDetail[0].id, 1)
                             if (result === true) {
                                 res.status(200).json({
                                     status: "Success",
@@ -59,22 +66,88 @@ module.exports = {
                                     message: "Save service relation failed"
                                 })
                             }
+                        } else {
+                            res.status(202).json({
+                                status: "Failed",
+                                message: "Item is not available: " + donorServiceId
+                            })
                         }
                     }
 
 
-                } else {
-                    res.status(202).json({
-                        status: "Failed",
-                        message: "Service is unavailable"
-                    })
                 }
+
             } else {
                 res.status(202).json({
                     status: "Failed",
-                    message: "Not found service detail"
+                    message: "Not found service ID: " + donorServiceId
                 })
             }
+
+
+
+            // const serviceDetailFound = await serviceDetailService.getServiceDetailInformationById(serviceDetailId)
+            // if (serviceDetailFound.length > 0) {
+            //     if (serviceDetailFound[0].statusId !== 4) {
+            //         const donorServiceFound = await donorServiceService.getServiceById(serviceDetailFound[0].serviceId)
+            //         console.log(donorServiceFound[0])
+            //         if (donorServiceFound.length > 0) {
+            //             if (donorServiceFound[0].serviceTypeId === 3) {
+            //                 //hien vat
+            //                 const quantityDB = serviceDetailFound[0].quantity
+            //                 if (quantityDB >= quantity) {
+            //                     await serviceDetailService.updateServiceDetailQuantity(serviceDetailId, (quantityDB - quantity))
+            //                     if (quantityDB === quantity) {
+            //                         const unavailableStatus = 4
+            //                         await serviceDetailService.updateDetailStatusById(serviceDetailId, unavailableStatus)
+            //                     }
+            //                     const result = await donorServiceRelationAccountService.saveServiceRelationToAccount(emailReceive, serviceDetailId, quantity)
+            //                     if (result === true) {
+            //                         // tru so luong
+            //                         res.status(200).json({
+            //                             status: "Success",
+            //                             message: "Save service relation successfully"
+            //                         })
+            //                     } else {
+            //                         res.status(202).json({
+            //                             status: "Failed",
+            //                             message: "Save service relation failed"
+            //                         })
+            //                     }
+            //                 }
+
+            //             } else {
+            //                 // code
+            //                 const unavailableStatus = 4
+            //                 await serviceDetailService.updateDetailStatusById(serviceDetailId, unavailableStatus)
+            //                 const result = await donorServiceRelationAccountService.saveServiceRelationToAccount(emailReceive, serviceDetailId, 1)
+            //                 if (result === true) {
+            //                     res.status(200).json({
+            //                         status: "Success",
+            //                         message: "Save service relation successfully"
+            //                     })
+            //                 } else {
+            //                     res.status(202).json({
+            //                         status: "Failed",
+            //                         message: "Save service relation failed"
+            //                     })
+            //                 }
+            //             }
+            //         }
+
+
+            //     } else {
+            //         res.status(202).json({
+            //             status: "Failed",
+            //             message: "Service is unavailable"
+            //         })
+            //     }
+            // } else {
+            //     res.status(202).json({
+            //         status: "Failed",
+            //         message: "Not found service detail"
+            //     })
+            // }
 
 
         } catch (error) {
